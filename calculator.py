4d395c6ad4c6f67f4c15840510fd12704c900250
Nell44577642 <nell.ribeiro@esme.fr>
2025-12-12 16:54:02 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,4 +4,7 @@ def multiply(n1, n2):
     return n1*n2
 def divide(n1,n2):
     return n1/n2
+def substract(n1,n2):
+    return n1 - n2
+    
 # TODO: Add more functions below this line (subtract, multiply, divide)
