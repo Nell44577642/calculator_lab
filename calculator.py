d9802b7bf406a2825eb5da26855dd58a142f55a6
kana2006 <kana.ito-russo@esme.fr>
2025-12-12 16:53:07 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 def add(n1, n2):
     return n1 + n2
-
+def multiply(n1, n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2
 # TODO: Add more functions below this line (subtract, multiply, divide)
